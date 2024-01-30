bad69421586848332a6b00cba5c5213d7c8755a8
sergio <sergioalejandrogutierrezmedina@gmail.com>
2024-01-30 12:21:06 -0500

    updates in header

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
-const menuEmail = document.querySelector('.flechita')
-const desktopMenu = document.querySelector('.desktop-menu')
+//opciones en header al hacer touch correo
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
 
-menuEmail.addEventListener('click', toggleDesktopMenu)
+menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');//Este cierra
+    mobileMenu.classList.add('inactive');
+
+    desktopMenu.classList.toggle('inactive');//Este abre 
 }
 
-console.log("Funcionando")
+//mobile menu
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    desktopMenu.classList.add('inactive');
+    aside.classList.add('inactive');
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+
+//detalles de carrito
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
+
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleCarritoAside() {
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+
+    aside.classList.toggle('inactive');
+}
